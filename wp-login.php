3e98a22fd2f66e432636a6b96d2e9fc4b76c13fb
minhtri5613 <85531513+minhtri5613@users.noreply.github.com>
2021-06-15 14:57:12 +0700

    y

--- wp-login.php
+++ wp-login.php
@@ -85,7 +85,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/minhtri\/wp-admin\/admin-ajax.php"}};
 </script>
 <script src="https://minhtri5613.github.io/tuilatri/wp-includes/js/wp-util.min.js?ver=5.7.2" id="wp-util-js"></script>
 <script id="user-profile-js-extra">
-var userProfileL10n = {"user_id":"0","nonce":"1dca0e7821"};
+var userProfileL10n = {"user_id":"0","nonce":"89c404b116"};
 </script>
 <script id="user-profile-js-translations">
 ( function( domain, translations ) {
